1604433a2a5e61b5fa896cc709085e01de2277a9
Abayomi Apetu <abayomismart@gmail.com>
2019-10-19 10:21:22 +0100

    updated app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                                         document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('users.show',Auth::user->id) }}">
+                                    <a class="dropdown-item" href="{{ route('users.show',Auth::user()->id) }}">
                                         {{ __('Profile Settings') }}
                                     </a>
 
